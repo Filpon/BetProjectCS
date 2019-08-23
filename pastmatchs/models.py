fd4b1aa2d523c6db88c78d97519e8e57e2717cd0
Filipp Ponomarev <filpon@mail.ru>
2019-08-24 00:21:35 +0300

    Update models.py

--- pastmatchs/models.py
+++ pastmatchs/models.py
@@ -3,14 +3,14 @@ from django.db import models
 from django.contrib.auth.decorators import permission_required
 from django.utils import timezone
 
-class Cesvpastmatchs(models.Model):
-    Left_team_title  = models.CharField(max_length=50)
-    Moneybet_for_left_team = models.IntegerField(default=0)
-    Score = models.CharField(max_length=50)
-    Right_team_title = models.CharField(max_length=50)
-    Moneybet_for_right_team = models.IntegerField(default=0)
-    Winning_team_match_result = models.CharField(max_length=50)
-    Hypothesis = models.CharField(max_length=50)
+class CesvPastMatchs(models.Model):
+    left_team_title  = models.CharField(max_length=50)
+    moneybet_for_left_team = models.IntegerField(default=0)
+    score = models.CharField(max_length=50)
+    right_team_title = models.CharField(max_length=50)
+    moneybet_for_right_team = models.IntegerField(default=0)
+    winning_team_match_result = models.CharField(max_length=50)
+    hypothesis = models.CharField(max_length=50)
 
     def __str__(self):
-        return f'{self.Left_team_title} {self.Right_team_title}'
+        return f'{self.left_team_title} {self.right_team_title}'
