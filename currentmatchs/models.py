8a011d9b736763fa803f947ef700b3bb416c3c16
Filipp Ponomarev <filpon@mail.ru>
2019-08-24 00:05:02 +0300

    Update models.py

--- currentmatchs/models.py
+++ currentmatchs/models.py
@@ -1,17 +1,17 @@
 from django.conf import settings
 from django.db import models
 
-class Cesvcurrentmatchs(models.Model):
-    Left_team_title = models.CharField(max_length=50)
-    Left_team_coefficients = models.CharField(max_length=50)
-    Moneybet_for_left_team = models.IntegerField(default=0)
-    Left_team_win_percent = models.CharField(max_length=50)
-    Time_before_match = models.CharField(max_length=50)
-    Right_team_title = models.CharField(max_length=50)
-    Right_team_coefficients = models.CharField(max_length=50)
-    Moneybet_for_right_team = models.IntegerField(default=0)
-    Right_team_win_percent = models.CharField(max_length=50)
+class CesvCurrentMatchs(models.Model):
+    left_team_title = models.CharField(max_length=50)
+    left_team_coefficients = models.CharField(max_length=50)
+    moneybet_for_left_team = models.IntegerField(default=0)
+    left_team_win_percent = models.CharField(max_length=50)
+    time_before_match = models.CharField(max_length=50)
+    right_team_title = models.CharField(max_length=50)
+    right_team_coefficients = models.CharField(max_length=50)
+    moneybet_for_right_team = models.IntegerField(default=0)
+    right_team_win_percent = models.CharField(max_length=50)
 
     def __str__ (self):
-        return f'{self.Left_team_title} {self.Right_team_title}'
+        return f'{self.left_team_title} {self.right_team_title}'
 
