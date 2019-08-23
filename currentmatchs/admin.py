fcab9d493d69e5eae216ac10dcb9bc07561b5080
Filipp Ponomarev <filpon@mail.ru>
2019-08-24 00:13:24 +0300

    Update admin.py

--- currentmatchs/admin.py
+++ currentmatchs/admin.py
@@ -1,4 +1,4 @@
 from django.contrib import admin
-from .models import Cesvcurrentmatchs
+from .models import CesvCurrentMatchs
 
-admin.site.register(Cesvcurrentmatchs)
+admin.site.register(CesvCurrentMatchs)
